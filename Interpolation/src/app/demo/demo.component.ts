03d6b8555d96e86912ce73bb7fde2f2247e0ca50
Shivam Bharambe <84351597+shivam-bharambe@users.noreply.github.com>
2024-02-05 16:07:32 +0530

    Add files via upload

---
+++ Interpolation/src/app/demo/demo.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-demo',
+  standalone: true,
+  imports: [],
+  templateUrl: './demo.component.html',
+  styleUrl: './demo.component.css'
+})
+export class DemoComponent 
+{
+  public Technology = "Angular";
+
+  public Display()
+  {
+    return "Marvellous Infosystem";
+  }
+}
